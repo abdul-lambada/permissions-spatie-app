21ca6a7a66fbc4c15ee5c278b954ec28dc990d9e
Abdul Kholik <akl859349@gmail.com>
2025-01-02 12:39:20 +0700

    create permissions

---
+++ resources/views/permissions/list.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Permisions') }}
+            </h2>
+        <a href="{{route('permissions.create')}}" class="bg-slate-700 hover:bg-slate-500 text-sm rounded-md px-5 py-1 text-white">Create</a>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-message></x-message>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
